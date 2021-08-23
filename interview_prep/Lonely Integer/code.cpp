ed7efb81b50b57eda13b0ee65cc3f7c95ce23fba
TanishAggarwal5 <aggarwaltanish27@gmail.com>
2021-08-23 17:01:53 +0530

    code cleaned

--- interview_prep/Lonely Integer/code.cpp
+++ interview_prep/Lonely Integer/code.cpp
@@ -18,11 +18,11 @@ int lonelyinteger(vector<int> a)                      //function to find the int
     vector<int> k(100,0);
 
     for (int i = 0; i<a.size(); i++) {                // creating a new vector of size of max element present in given vector, and initializing
-        k[a[i]]++;                                    // with 0, and whatever number occurs in main vector , increament the value by 1 at that index in vector we created.
+        k[a[i]]++;                                    // with 0 , and whatever number occurs in main vector , increament the value by 1 at that index in vector we created .
     }
-    
+
     for (int j = 0; j<k.size(); j++) {                // in the end, if the value at a particular index is 1 in the vector we ceated , then it means , number of that index occurred only once in given 
-        if (k[j]==1) {                                // vector and the index itself is the answer for our problem
+        if (k[j]==1) {                                // vector and the index itself is the answer for our problem .
             return j;
             break;
         }
