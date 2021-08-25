d8f2b4515d10aba2b9316c951f291d96f1f8e5b6
TanishAggarwal5 <aggarwaltanish27@gmail.com>
2021-08-25 11:27:35 +0530

    Input output issue #11 fixed

--- cpp/Input_and_Output/code.cpp
+++ cpp/Input_and_Output/code.cpp
@@ -1,21 +1,21 @@
 #include <cmath>                 
 #include <cstdio>
 #include <vector>
-#include <iostream>      //main header file for input out stream 
+#include <iostream>            //main header file for input output stream 
 #include <algorithm>
 using namespace std;
 
 
 int main() {                   //start of main function
 
-    int a,b,c,x;             //declaring 3 variables
+    int a,b,c,x;               //declaring 3 variables
 
-    cin>>a>>b>>c;                 // taking input
+    cin>>a>>b>>c;              // taking input
 
-    x = a +b +c;             // storing sum in x
+    x = a + b + c;             // storing sum in x
 
-    cout<<x;                   // printing x or sum
+    cout<<x;                   // printing sum
     
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+      
     return 0;
 }
