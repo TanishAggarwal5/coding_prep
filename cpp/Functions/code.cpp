05d83dd52bc07041eee0e31f0222a0c474a1da10
TanishAggarwal5 <aggarwaltanish27@gmail.com>
2021-08-25 10:36:47 +0530

    Functions issue #8 fixed

--- cpp/Functions/code.cpp
+++ cpp/Functions/code.cpp
@@ -5,27 +5,22 @@ using namespace std;
 /*
 Add `int max_of_four(int a, int b, int c, int d)` here.
 */
-int max_of_four(int a,int b,int c,int d) // function to compare
+int max_of_four(int a,int b,int c,int d)   //function to compare
 {
     if(a>b && a>c && a>d)
-    {
-        return a;  //return a if its greater than every other number
-    }
+        return a;                          //return a if its greater than every other number
+    
     else if(b>c && b>d)
-    {
-        return b;  // if a not greatest then compare  next number with remaining numbers
-    }
-    else if (c>d) {
-        return c;    // if b not greatest then compare  next number with remaining numbers 
-    }
+        return b;                          //if a not greatest then compare  next number with remaining numbers
+    
+    else if (c>d) 
+        return c;                          //if b not greatest then compare  next number with remaining numbers 
+    
     else
-    return d;        //in end if none of first three is greatest than result is d only
-        
-    
-    
+        return d;                          //in end if none of first three is greatest than result is d only
 }
 
-int main() {   //main fucntion
+int main() {                               //main fucntion
 
     //input of 4 number to compare
     int a, b, c, d;
