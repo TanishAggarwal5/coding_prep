cc18635008387e39ea1e14061c28d854201925d9
TanishAggarwal5 <aggarwaltanish27@gmail.com>
2021-08-25 11:40:44 +0530

    Pointers issue #12 fixed

--- cpp/Pointer/code.cpp
+++ cpp/Pointer/code.cpp
@@ -1,25 +1,30 @@
-#include <stdio.h>          // solution in c with header stdio.h
+// solution in c with header stdio.h
+#include <stdio.h>          
 
-//updatefunction of return type void
-void update(int *a,int *b) {
-    //storing valueat address of pointer a in temp
+//update function of return type void
+void update(int *a,int *b) 
+{
+    //storing value at a in temp
     int temp = *a;
 
     //updating  value at a with sum of value at a and value at b 
-    *a =*a + *b;
+    *a = *a + *b;
 
     //using if statements to get absolute value for updating b same as a but with subtracted value
-    if (temp>>*b){
-        *b=temp - *b;
+    if (temp >> *b)
+    {
+        *b = temp - *b;
     }
-    else {
-        *b=*b - temp;
+    else 
+    {
+        *b = *b - temp;
     }
-    // Complete this function    
+      
 }
 //main function
-int main() {            
-    //decalring two variables
+int main() 
+{            
+    //declaring two variables
     int a, b;
 
     //storing their addresses in two pointers respectively
